26f767b7e3d62bdda27f20ebc03b8db83e173ec1
Daniel Gray <daniel.gray@puregym.com>
2025-03-10 18:08:35 +0000

    It works, not perfect yet though

--- pipypixels/games/bounce.py
+++ pipypixels/games/bounce.py
@@ -8,23 +8,23 @@ from pipypixels.graphics.shared import Matrix
 class BounceEngine(VectorGameEngine):
     def __init__(self, scale, matrix: Matrix, frame_rate):
         super().__init__(scale, matrix, frame_rate)
-        self.__ball_pos = (float(0),float(0))
-        self.__ball_vector = (float(0),float(0))
+        self.__balls = []
 
     def reset(self):
         self.board.reset()
         self.__draw_walls()
-        #self.__ball_pos = self.board.get_random_empty_position()
-        #self.__ball_vector = (randrange(0,360),0.5)
-        self.__ball_pos = (32,32)
-        self.__ball_vector = (240,0.5)
-        self.__draw_ball()
+        self.__spawn_balls(100)
 
-    def __clear_ball(self):
-        self.board.set(int(self.__ball_pos[0]), int(self.__ball_pos[1]), GameEntity.EMPTY)
+    def __spawn_balls(self, count:int):
+        for _ in range(count):
+            ball = (self.board.get_random_empty_position(),(randrange(0,360),0.5))
+            self.__balls.append(ball)
 
-    def __draw_ball(self):
-        self.board.set(int(self.__ball_pos[0]), int(self.__ball_pos[1]), GameEntity.BALL)
+    def __clear_ball(self, position:(float,float)):
+        self.board.set(int(position[0]), int(position[1]), GameEntity.EMPTY)
+
+    def __draw_ball(self, position:(float,float)):
+        self.board.set(int(position[0]), int(position[1]), GameEntity.BALL)
 
     def _colour_cell_func(self, x, y, entity_type):
         colour = [0,0,0]
@@ -44,41 +44,50 @@ class BounceEngine(VectorGameEngine):
 
     def _game_tick(self):
         # determine next position
-        next_pos_and_vector = self.__calculate_next_position_and_vector()
-        # __clear_ball
-        self.__clear_ball()
-        # update ball pos
-        self.__ball_pos = next_pos_and_vector[0]
-        self.__ball_vector = next_pos_and_vector[1]
-        # __draw_ball
-        self.__draw_ball()
+        for i in range(len(self.__balls)):
+            ball = self.__balls[i]
+            next_pos_and_vector = self.__calculate_next_position_and_vector(ball[0], ball[1])
+            # __clear_ball
+            self.__clear_ball(ball[0])
+            # update ball pos
+            self.__balls[i] = next_pos_and_vector
+            # __draw_ball
+            self.__draw_ball(next_pos_and_vector[0])
 
-    def __calculate_next_position_and_vector(self)->((float,float),(float,float)):
-        a = self.__ball_vector[0]
-        v = self.__ball_vector[1]
-        x = self.__ball_pos[0]
-        y = self.__ball_pos[1]
-        nx = x + (math.cos(a) * v)
-        ny = y + (math.sin(a) * v)
+    @staticmethod
+    def __angle_overflow(angle:float)->float:
+        if angle < 0:
+            return angle + 360
+        if angle >= 360:
+            return angle - 360
+        return angle
+
+    def __calculate_next_position_and_vector(self, position:(float, float), vector:(float, float))->((float,float),(float,float)):
+        a = vector[0]
+        v = vector[1]
+        x = position[0]
+        y = position[1]
+        nx = x + (math.cos(math.radians(a - 90)) * v)
+        ny = y + (math.sin(math.radians(a - 90)) * v)
         my = self.board.height() / 2
         mx = self.board.width() / 2
+        dx = mx - x
+        dy = my - y
         if nx <= 1:
             nx = 1
-            d = abs(my - ny)
-            na = a + 180
-            if na > 360:
-                na = na - 360
-            elif na < 0:
-                na = na + 360
+            a = self.__angle_overflow(360 - a) + dy
 
         if nx >= self.board.width() - 2:
             nx = self.board.width() - 2
+            a = self.__angle_overflow(360 - a) + dy
 
         if ny <= 1:
             ny = 1
+            a = self.__angle_overflow(180 - a) + dx
 
         if ny >= self.board.height() - 2:
             ny = self.board.height() - 2
+            a = self.__angle_overflow(180 - a) + dx
 
         return (nx, ny),(a, v)
 
@@ -87,4 +96,4 @@ class BounceScreen(GameScreen):
         super().__init__(matrix, self.__get_engine, redraw_on_show=True)
 
     def __get_engine(self) ->GameEngine:
-        return BounceEngine(self._scale, self._matrix, 24)
+        return BounceEngine(self._scale, self._matrix, 100)
