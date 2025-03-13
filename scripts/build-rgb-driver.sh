dcddab26b64350cd4915dff6a1291ca7fcf43094
Daniel Gray <daniel.gray@puregym.com>
2025-03-13 17:59:18 +0000

    Clone not checkout you old fool

--- scripts/build-rgb-driver.sh
+++ scripts/build-rgb-driver.sh
@@ -2,7 +2,7 @@
 
 pushd ..
 # TODO:Get the python version to use when copying later on...
-git checkout https://github.com/hzeller/rpi-rgb-led-matrix.git rpi-rgb-led-matrix
+git clone https://github.com/hzeller/rpi-rgb-led-matrix.git rpi-rgb-led-matrix
 pushd rpi-rgb-led-matrix/bindings/python
 make build-python
 cp bindings/python/rgbmatrix ../../../PiPyPixels/lib/python3.11/site-packages/rgbmatrix
