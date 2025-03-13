adf5f639d059cf34e25a5a906034429379ed477b
Daniel Gray <daniel.gray@puregym.com>
2025-03-13 17:58:21 +0000

    BASH!

--- scripts/build-rgb-driver.sh
+++ scripts/build-rgb-driver.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-pushd .. >> dev/null
+pushd ..
 # TODO:Get the python version to use when copying later on...
 git checkout https://github.com/hzeller/rpi-rgb-led-matrix.git rpi-rgb-led-matrix
-pushd rpi-rgb-led-matrix/bindings/python >> dev/null
+pushd rpi-rgb-led-matrix/bindings/python
 make build-python
 cp bindings/python/rgbmatrix ../../../PiPyPixels/lib/python3.11/site-packages/rgbmatrix
-popd >> dev/null
-popd >> dev/null
+popd
+popd
